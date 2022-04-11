29999a9732da6e2625f23704a6112c98817ba5be
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-11 23:29:55 +0600

    router setup and firebase setup

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,6 +7,7 @@ const Header = () => {
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/login">Login</Link>
+                <Link to="/login">Login</Link>
 
             </nav>
 
