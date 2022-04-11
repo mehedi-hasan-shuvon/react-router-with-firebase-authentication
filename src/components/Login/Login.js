13ecb4c1c5d036aea93c040fd6432224af1bed5e
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-12 02:25:52 +0600

    added required navication system with location

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -2,18 +2,31 @@ import { getAuth } from 'firebase/auth';
 import React from 'react';
 // import useFirebase from '../../hooks/useFirebase';
 import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { useLocation, useNavigate } from 'react-router-dom';
 import app from '../../firebase.init';
 import './Login.css'
 
 const auth = getAuth(app);
 const Login = () => {
     // const { signInWithGoogle } = useFirebase();
+    const location = useLocation();
+    const navigate = useNavigate();
     const [signInWithGoogle, user] = useSignInWithGoogle(auth);
+    const from = location?.state?.from?.pathname || "/";
+
+
+    const handelGoogleSignIn = () => {
+        signInWithGoogle()
+            .then(() => {
+                navigate(from, { replace: true });
+            })
+    };
+
     return (
         <div>
             <h3>please login</h3>
             <div className='google-signin'>
-                <button onClick={() => signInWithGoogle()}>Sing in using Google</button>
+                <button onClick={handelGoogleSignIn}>Sing in using Google</button>
             </div>
             <form>
                 <input type="email" name="" id="" placeholder='Your Email' />
