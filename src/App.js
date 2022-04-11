3e317c902bf2684ac1d3fae1eff0d9ffe2fbed9b
mehedi <mehedi.hasan.shuvon@gmail.com>
2022-04-12 02:15:14 +0600

    intregae required auth system for protecting a componenet

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Header from './components/Header/Header';
 import Register from './components/Register/Register';
 import Products from './components/Products/Products';
 import Orders from './components/Orders/Orders';
+import RequireAuth from './components/RequireAuth/RequireAuth';
 
 function App() {
   return (
@@ -17,7 +18,10 @@ function App() {
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
         <Route path='/products' element={<Products></Products>}></Route>
-        <Route path='/orders' element={<Orders></Orders>}></Route>
+        <Route path='/orders' element={
+          <RequireAuth>
+            <Orders></Orders>
+          </RequireAuth>}></Route>
       </Routes>
 
     </div>
